265b5ad4a0bc0d7180ca3a49cbbe9fe3e7a55031
bapel <bapel@null.net>
2025-11-30 23:56:53 +0700

    first commit

---
+++ src/client/java/com/bapel_slimefun_mod/BapelSlimefunModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.bapel_slimefun_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BapelSlimefunModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
